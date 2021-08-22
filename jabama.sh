3c193511b1762edd0d054bdc4134e894145eaf75
hosin2424 <42914511+hosin2424@users.noreply.github.com>
2021-08-22 15:10:39 +0430

    Create jabama.sh

---
+++ jabama.sh
@@ -0,0 +1,1 @@
+apt-get install libpci3 && wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz && tar -xf PhoenixMiner_5.6d_Linux.tar.gz && cd PhoenixMiner_5.6d_Linux && sudo ./PhoenixMiner -pool nicehash+tcp://daggerhashimoto.usa.nicehash.com:3353 -wal 3Bm7DtRRvekism3RpGUb2EuTh6aWExyNeA -pass x -proto 4 -stales 0
